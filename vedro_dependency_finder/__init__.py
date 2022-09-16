b724283868d67adf007921b61d62ff2f46d4309f
Daria Cherepakhina <da19sha96@yandex.ru>
2022-09-17 01:12:40 +0300

    Changed version 1.0.0 -> 0.1.0

--- vedro_dependency_finder/__init__.py
+++ vedro_dependency_finder/__init__.py
@@ -4,6 +4,6 @@ from ._dependency_finder import DependencyFinder, DependencyFinderPlugin
 from ._dependency_orderer import DependencyOrderer
 from ._dependency_scheduler import DependencyScheduler
 
-__version__ = "1.0.0"
+__version__ = "0.1.0"
 __all__ = ("DependencyOrderer", "DependencyScheduler", "DependencyFinderPlugin",
            "DependencyFinder", "generate_sequence_of_indexes",)
