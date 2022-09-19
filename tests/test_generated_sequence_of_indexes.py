426235d4083b771c4f09b9294587f9ae2b4d8d97
Daria Cherepakhina <da19sha96@yandex.ru>
2022-09-19 15:03:09 +0300

    Added test

--- tests/test_generated_sequence_of_indexes.py
+++ tests/test_generated_sequence_of_indexes.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 from baby_steps import given, then, when
 
 from vedro_dependency_finder._generate_sequence_of_indexes import generate_sequence_of_indexes
@@ -25,6 +27,18 @@ def test_sequence_with_two_diff_indexes():
 
     with then:
         assert sequence == [5, 0, 5, 1, 5, 2, 5, 3, 5, 4, 0, 4, 1, 4, 2, 4, 3, 4, 5]
+
+
+def test_sequence_when_indexes_are_the_same():
+    with given:
+        all_indexes = list(range(0, 3))
+        diff_indexes = deepcopy(all_indexes)
+
+    with when:
+        sequence = generate_sequence_of_indexes(all_indexes, diff_indexes)
+
+    with then:
+        assert sequence == [2, 0, 2, 1, 0, 1, 2]
 
 
 def test_sequence_without_all_indexes():
